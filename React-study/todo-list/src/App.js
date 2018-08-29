bf62a15bc3a0d71ce95835ddf6edb004d3be6191
ssamwonn <ssamwonn@ssamwonnui-MacBook-Pro.local>
2018-08-30 01:49:16 +0900

    컴포넌트 구상하기 중간(수정)

--- React-study/todo-list/src/App.js
+++ React-study/todo-list/src/App.js
@@ -5,26 +5,12 @@ import TodoItemList from './components/TodoItemList';
 
 
 class App extends Component {
-  
   (...)
 
-  handleToggle = (id) => {
+  handleRemove = (id) => {
   const { todos } = this.state;
-
-  // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
-  const index = todos.findIndex(todo => todo.id === id);
-  const selected = todos[index]; // 선택한 객채
-  
-  const nextTodos = [...todos]; // 배열을 복사 
-   
-  // 기존의 값들을 복사하고, checked 값을 덮어쓰기 
-  nextTodos[index] = {
-    ...selectd,
-    checked: !seleted.checked
-  };
-  
   this.setState({
-    todos: nextTodos
+    todos: todos.filter(todo => todo.id !== id)
   });
 }
 
@@ -34,9 +20,9 @@ render() {
     handleChange,
     handleCreate,
     handleKeyPress,
-    handleToggle
+    handleToggle,
+    handleRemove
   } = this;
-   
 
   return (
     <TodoListTemplate form={(
@@ -47,7 +33,7 @@ render() {
         onCreate={handleCreate}
       />
     )}>
-      <TodoItemList todos={todos} />
+      <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
     </TodoListTemplate>
   );
 }
