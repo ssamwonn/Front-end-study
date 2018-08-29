c7202f92da2d7567da216988d714352d380abfba
ssamwonn <ssamwonn@ssamwonnui-MacBook-Pro.local>
2018-08-30 01:45:44 +0900

    컴포넌트구상하기 중간

--- React-study/todo-list/src/App.js
+++ React-study/todo-list/src/App.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
-import Form from  './components/Form';
+import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 
 class App extends Component {
-  render() {
-    return (
-      <TodoListTemplate form={<Form/>}>
-       <TodoItemList/>
-      </TodoListTemplate>
-    );
-  }
+  
+  (...)
+
+  handleToggle = (id) => {
+  const { todos } = this.state;
+
+  // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+  const index = todos.findIndex(todo => todo.id === id);
+  const selected = todos[index]; // 선택한 객채
+  
+  const nextTodos = [...todos]; // 배열을 복사 
+   
+  // 기존의 값들을 복사하고, checked 값을 덮어쓰기 
+  nextTodos[index] = {
+    ...selectd,
+    checked: !seleted.checked
+  };
+  
+  this.setState({
+    todos: nextTodos
+  });
+}
+
+render() {
+  const { input, todos } = this.state;
+  const {
+    handleChange,
+    handleCreate,
+    handleKeyPress,
+    handleToggle
+  } = this;
+   
+
+  return (
+    <TodoListTemplate form={(
+      <Form
+        value={input}
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+        onCreate={handleCreate}
+      />
+    )}>
+      <TodoItemList todos={todos} />
+    </TodoListTemplate>
+  );
+}
 }
 
 export default App;
