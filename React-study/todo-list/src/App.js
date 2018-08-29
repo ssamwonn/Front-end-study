8b412b94e51197174a97dc00a6a80c25137d6f44
ssamwonn <ssamwonn@ssamwonnui-MacBook-Pro.local>
2018-08-29 17:44:39 +0900

    컴포넌트 구성하기

--- React-study/todo-list/src/App.js
+++ React-study/todo-list/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from  './components/Form';
+import TodoItemList from './components/TodoItemList';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <TodoListTemplate form={<Form/>}>
+       <TodoItemList/>
+      </TodoListTemplate>
     );
   }
 }
